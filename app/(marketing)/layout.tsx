737d20e70e5d007eeace20a53f3d829d025b96c5
Rajan14-11 <rajangoyal125@gmail.com>
2024-05-13 16:18:28 +0530

    changed db to mongo

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Footer } from "./_components/footer";
+import Navbar from "./_components/navbar";
+
+const MarketinLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="bg-slate-100 h-full">
+        <Navbar/>
+      <main className="pt-40 pb-20 bg-slate-100">{children}</main>
+      <Footer/>
+    </div>
+  );
+};
+
+export default MarketinLayout;
